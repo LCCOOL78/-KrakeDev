b433191633c20e8394adadd3c7698bc574c8b48b
LeandroCool <elreycool78@gmail.com>
2025-09-17 11:27:37 -0500

    el boton modifica la imagen

--- ejercicio git/ejercicio.js
+++ ejercicio git/ejercicio.js
@@ -1,3 +1,7 @@
 saludar=function(){
+    let cmpimagenSaludo;
+    cmpimagenSaludo=document.getElementById("imgsaludo")
+    cmpimagenSaludo.src="./IMAGENES/ganar 2.gif"
     console.log("ingresa a saludar");
+
 }
